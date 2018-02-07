7ba371d050f6b861f781313d78c28cfedaf0db37
Guillermo A. Fernández Kessler <guillermo.kessler@redb.ee>
2018-02-07 19:04:53 -0300

    encrypt

---
+++ src/main/java/com/rfsc/cloud/config/configuration/ConfigDevelopment.java
@@ -0,0 +1,23 @@
+package com.rfsc.cloud.config.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Profile({"local", "default"})
+@Slf4j
+public class ConfigDevelopment extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        log.info("Configuracion de seguridad para DESARROLLO");
+        http.csrf()
+            .disable()
+            .authorizeRequests()
+            .antMatchers("/**")
+            .permitAll();
+    }
+}
